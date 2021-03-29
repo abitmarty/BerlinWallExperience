b256f60fff81d496954c86112a1426b786672a2f
abitmarty <matijn.totte@gmail.com>
2021-03-29 23:17:48 +0200

    Added sound and panning reviced

--- src/music.js
+++ src/music.js
@@ -27,11 +27,11 @@ var panner = new StereoPannerNode(audioCtx, pannerOptions);
 // Sound blocks 
 // Simply import the audio and put them in the targetedBlocks array (make sure there is at least 2 free spaces between audio files)
 var targetedBlocks = [];
-var lastTargetedBlock = 1000;
-targetedBlocks.key3 = trabant;
-targetedBlocks.key7 = dakota;
-targetedBlocks.key11 = bowie;
-targetedBlocks.key13 = voice2;
+var lastTargetedBlock = "";
+targetedBlocks.trabant = trabant;
+//targetedBlocks.key7 = dakota;
+//targetedBlocks.key11 = bowie;
+//targetedBlocks.key13 = voice2;
 
 // Var
 var audioSource;
@@ -106,56 +106,46 @@ export function playMusic(pos, slideWidth, scrollDirection, slidePadding, mode){
     //         fade = 0;
     //     }
     // }
+    playAudioImage();
 }
 
-function playAudio(slideWidth){
+function playAudioImage() {
+    $('.js-sound').each(function () {
+        var posLeft = $($('.js-sound')).offset().left;
+        var itemWidth = $($('.js-sound')).outerWidth();
+        var soundSource = $(this).find('img');
+        if (posLeft < $(window).width() && posLeft+itemWidth > 0) {
+            playAudio(posLeft, itemWidth, soundSource);
+        } else {
+            stopAudio();
+        }
+    });
+}
+
+function playAudio(posLeft, itemWidth, soundSource){
     // check if context is in suspended state (autoplay policy)
 	// if (audioCtx.state === 'suspended') {
 	// 	audioCtx.resume();
 	// }
 
     // Calculate gain
-    var gainBuildUp = (Math.round((100/slideWidth) * positionTry) / 100);
-    var gainBuildDown = 1-gainBuildUp;
-
-    var targetedContentBlock;
-    if (targetedBlocks.hasOwnProperty("key" + contentBlock)){
-        targetedContentBlock = contentBlock;
-        setAudioSource(targetedContentBlock);
-    } else if (targetedBlocks.hasOwnProperty("key" + (contentBlock + 1))){
-        targetedContentBlock = contentBlock + 1;
-        setAudioSource(targetedContentBlock);
-    } else if (targetedBlocks.hasOwnProperty("key" + (contentBlock - 1))){
-        targetedContentBlock = contentBlock - 1;
-        setAudioSource(targetedContentBlock);
-    }else {
-        // Unreal value
-        targetedContentBlock = 1000;
+    var percentagePosition = Math.round((100/($(window).width()+itemWidth))* (posLeft+itemWidth))/100;
+    var gainValue = percentagePosition * 2;
+    var panValue = gainValue - 1;
+    if (percentagePosition > 0.5){
+        gainValue = 2 - gainValue;
     }
+    gainNode.gain.value = gainValue;
+    panner.pan.value = panValue;
 
-    // The block before the targeted block
-    if(contentBlock == (targetedContentBlock - 1)){
-        gainNode.gain.value = gainBuildUp * (fade/100);
-        panner.pan.value = gainBuildDown;
-        if(audioSource.paused){
-            // Play audio after interaction with DOM
-            audioSource.play().catch(function(error) { });
-        }
-    } else if(contentBlock == targetedContentBlock){
-        gainNode.gain.value = 1 * (fade/100);
-        panner.pan.value = 0;
-        if(audioSource.paused){
-            audioSource.play().catch(function(error) { });
-        }
-    } else if(contentBlock == (targetedContentBlock + 1)){
-        gainNode.gain.value = gainBuildDown * (fade/100);
-        panner.pan.value = 0 - gainBuildUp;
-        if(audioSource.paused){
-            audioSource.play().catch(function(error) { });
-        }
-    } else {
-        // Reset to start of audio
-        stopAudio();
+    // Set source
+    if (lastTargetedBlock != soundSource[0].className){
+        lastTargetedBlock = soundSource[0].className;
+        setAudioSource(soundSource);
+    }
+    if(audioSource.paused){
+        //console.log(audioSource.paused);
+        audioSource.play().catch(function(error) { });
     }
 }
 
@@ -168,65 +158,14 @@ function stopAudio(){
 }
 
 // Create new audio based on the position we're in
-function setAudioSource(targetedContentBlock){
-    if (lastTargetedBlock != targetedContentBlock){
-        pannerOptions = {pan: 0};
-        audioCtx = new AudioContext();
-        panner = new StereoPannerNode(audioCtx, pannerOptions);
-        gainNode = audioCtx.createGain();
-        audioSource = new Audio(targetedBlocks["key" + targetedContentBlock]);
-        audioSource.loop = true;
-        const trackSlide2 = audioCtx.createMediaElementSource(audioSource);
-        trackSlide2.connect(gainNode).connect(panner).connect(audioCtx.destination);
-    }
-    lastTargetedBlock = targetedContentBlock;
+function setAudioSource(soundSource){
+    pannerOptions = {pan: 0};
+    audioCtx = new AudioContext();
+    panner = new StereoPannerNode(audioCtx, pannerOptions);
+    gainNode = audioCtx.createGain();
+    audioSource = new Audio(targetedBlocks[soundSource[0].className]);
+    audioSource.loop = true;
+    const trackSlide2 = audioCtx.createMediaElementSource(audioSource);
+    trackSlide2.connect(gainNode).connect(panner).connect(audioCtx.destination);
 }
 
-// Get the content value of the center of the screen
-function setBlockContent(slidePadding){
-    // Count the amount of images
-    var container_div = document.getElementsByClassName('js-container');
-    var requiredContainer = container_div[0];
-    var count = requiredContainer.getElementsByClassName('js-slide').length;
-    var adjustedCount = count - (slidePadding * 2);
-
-    // Calculate what the content block is
-    if(blockCenter >= 2 && blockCenter <= (adjustedCount + slidePadding)){
-        contentBlock = blockCenter - 1;
-    }else if (blockCenter == 1){
-        contentBlock = adjustedCount;
-    } else{
-        contentBlock = 1;
-    }
-}
-
-// Get the block value of the center of the screen
-function setCenterBlock(pos, slideWidth, scrollDirection, posTry){
-    // On first time
-    if (isFirst){
-        isFirst = false;
-        setBlockNumber(posTry, slideWidth, scrollDirection);
-    }
-
-    // Sometimes position skips 0 so we check for a few low values
-    if (positionTry < safeValue) {
-        check = false;
-        setBlockNumber(posTry, slideWidth, scrollDirection);
-    }
-
-    // Due to the never 0 issue  we check the left over values
-    if(positionTry <= safeValue && !check && scrollDirection === 'left'){
-        positionTry = slideWidth;
-    }
-
-    // Adition to the function above
-    // Setting check
-    if (positionTry > safeValue) {
-        check = true;
-    }
-}
-
-function setBlockNumber(posTry, slideWidth, scrollDirection){
-    var blockNumber = Math.round(Math.abs(posTry) / slideWidth + (scrollDirection === 'right' ? 1 : 0));
-    blockCenter = blockNumber;
-}
